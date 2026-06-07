0fa7a891f1448fe9a9c9d048e32c141efea338df
Hari <hariharandevarajs@gmail.com>
2026-06-07 19:25:29 +0530

    Finished Forecase application errors , completed APP

--- main.py
+++ main.py
@@ -65,6 +65,41 @@ lets look at the backend.py file where api data extracter from api
 Filtering data from raw file in backend.py
 """
 
+# from backend import get_data
+# import streamlit as st
+# import plotly.express as px
+# st.title("Weather forecast for the next days")
+# place=st.text_input("Enter your place here: ")
+# days=st.slider("Forcast Days",min_value=1,max_value=5)
+# option = st.selectbox("select data to view",("TEMPERATURE","SKY"))
+# st.subheader(f"{option} for the next {days} days in {place}")
+#
+# #here we are going to use if condition , no need to use there in backend.py so remove them
+# if place: #if input box empty means we got some error like list error so we add place  condition ,
+#     # if place value exist only it would run further thats it
+#
+#     filtered_data = get_data(place, days)
+#     if option == "TEMPERATURE":
+#         temperature = [i['main']['temp'] for i in filtered_data]
+#         dates = [i['dt_txt'] for i in filtered_data]
+#         fiqure = px.line(x=dates, y=temperature, labels={'x': 'Date', 'y': 'Temperature(C)'})
+#         st.plotly_chart(fiqure)
+#
+#     if option == "SKY":
+#         images={'Clear':"images/clear.png",
+#                 'Clouds':"images/cloud.png",
+#                 'Rain':"images/rain.png",
+#                 'Snow':"images/snow.png"}
+#         sky_conditions = [i['weather'][0]['main'] for i in filtered_data]
+#         image_path=[images[i] for i in sky_conditions]
+#         st.image(image_path,width=115)
+
+"""
+student project fix the weather app error 
+1.showing temperature in full for example : 294, but actuall degree value 29.4 - so we need to change this first
+2.if entered location not exist, we get error so fix that , so tell them enter correct loaction
+"""
+
 from backend import get_data
 import streamlit as st
 import plotly.express as px
@@ -77,20 +112,30 @@ st.subheader(f"{option} for the next {days} days in {place}")
 #here we are going to use if condition , no need to use there in backend.py so remove them
 if place: #if input box empty means we got some error like list error so we add place  condition ,
     # if place value exist only it would run further thats it
+    try:
+        filtered_data = get_data(place, days)
+        if option == "TEMPERATURE":
+            temperature = [(i['main']['temp'])/10 for i in filtered_data]
+            dates = [i['dt_txt'] for i in filtered_data]
+            fiqure = px.line(x=dates, y=temperature, labels={'x': 'Date', 'y': 'Temperature(C)'})
+            st.plotly_chart(fiqure)
 
-    filtered_data = get_data(place, days)
-    if option == "TEMPERATURE":
-        temperature = [i['main']['temp'] for i in filtered_data]
-        dates = [i['dt_txt'] for i in filtered_data]
-        fiqure = px.line(x=dates, y=temperature, labels={'x': 'Date', 'y': 'Temperature(C)'})
-        st.plotly_chart(fiqure)
+        if option == "SKY":
+            images={'Clear':"images/clear.png",
+                    'Clouds':"images/cloud.png",
+                    'Rain':"images/rain.png",
+                    'Snow':"images/snow.png"}
+            sky_conditions = [i['weather'][0]['main'] for i in filtered_data]
+            dates = [i['dt_txt'] for i in filtered_data]
+            image_path=[images[i] for i in sky_conditions]
+            st.image(image_path,width=115,caption=dates)
+            st.text(dates)
 
-    if option == "SKY":
-        images={'Clear':"images/clear.png",
-                'Clouds':"images/cloud.png",
-                'Rain':"images/rain.png",
-                'Snow':"images/snow.png"}
-        sky_conditions = [i['weather'][0]['main'] for i in filtered_data]
-        image_path=[images[i] for i in sky_conditions]
-        st.image(image_path,width=115)
+    except KeyError:
+        st.error("Please enter correct location")
+    except AttributeError:
+        st.error("Please move the slide Further")
+
+
+
 
